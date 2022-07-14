e00a4483137d9ce7d8a692c08519d6663975f8ce
yutomizuta <yuto11752@i.softbank.jp>
2022-07-14 17:20:36 +0000

    7a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,10 @@ class BooksController < ApplicationController
 
   def index
     to = Time.current.at_end_of_day
-    from = (to - 6.day).at_beginning_day
-    @books = Book.includes(:favorited_users).
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_users).sort_by{|x| x.favorited_users.includes(:favorites).where(created_at: from...to).size}.reverse
     @book = Book.new
+    byebug
   end
 
   def create
