830acb4e66b32b51e463d349ece5eb0c6a676825
mizutayuto <yuto11752@i.softbank.jp>
2022-07-15 18:33:26 +0900

    7b

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,4 +9,15 @@ class Book < ApplicationRecord
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
+
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+
+  today = Time.current.end_of_day
+  last_week = (today - 6.day).beginning_of_day
+  scope :created_this_week, -> { where(created_at: last_week...today) }
+
+  last_week_first = (today - 13.day).beginning_of_day
+  last_week_end = (today - 7.day).end_of_day
+  scope :created_last_week, -> { where(created_at: last_week_first...last_week_end) }
 end
