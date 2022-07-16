ac29160b405b55ca7e0e8ac4769753d6397c2dee
mizutayuto <yuto11752@i.softbank.jp>
2022-07-16 23:39:51 +0900

    9a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,12 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+  impressionist :actions=> [:show]
 
   def show
     @book = Book.find(params[:id])
     @book_comment = BookComment.new
+    impressionist(@book, nil, unique: [:session_hash])
   end
 
   def index
--- app/models/book.rb
+++ app/models/book.rb
@@ -10,4 +10,6 @@ class Book < ApplicationRecord
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
+
+  is_impressionable
 end
